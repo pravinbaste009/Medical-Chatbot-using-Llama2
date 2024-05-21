1d834e12ec7e240548c4fcd91ec089edc73b01bb
pc <pravinbaste009@gmail.com>
2024-05-21 15:16:55 +0530

    last update

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from langchain_pinecone import PineconeVectorStore
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 from langchain.chains import RetrievalQA
+from pydantic import BaseModel
 import pinecone
 from dotenv import load_dotenv
 import os 
